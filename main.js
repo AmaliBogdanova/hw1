54529e89212893b4f129cbf431208b4775427048
amalia <ab14>
2022-10-06 19:42:37 +0500

    finish

---
+++ main.js
@@ -0,0 +1,16 @@
+var imya = prompt('введите ваше имя');
+console.log('Я ' + imya);
+var age = +prompt('введите ваш возвраст');
+console.log('Мне ' + age + ' лет');
+alert(age)
+
+var example1 = +prompt('112+88 = ?')
+console.log('Пример1:112+88='+(+112+88) + '  ваш ответ:'+ example1 ); 
+var example2 = +prompt('999-333 = ?')
+console.log('Пример1:999-333=' +(+999-333) + '  ваш ответ:'+ example2);
+var example3 = +prompt('45*34 = ?')
+console.log('Пример1:45*34=' + 45*34 + '  ваш ответ:'+ example3);
+var example4 = +prompt('33%6 = ?')
+console.log('Пример1:33%6=' + 33%6 + '  ваш ответ:'+ example4);
+var example5 = +prompt('233/23 = ?')
+console.log('Пример1:233/23=' + 233/23 + '  ваш ответ:'+ example5);
